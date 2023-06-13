f29263a536884f6a4382a59c536cfeae1ffddf1e
codedna <parkerpunj@gmail.com>
2023-06-13 04:45:00 -0400

    commit test

--- src/app/notes-api/controllers/groups-controller/groups-controller.ts
+++ src/app/notes-api/controllers/groups-controller/groups-controller.ts
@@ -27,6 +27,7 @@ export class GroupsController {
     }
     const updatedGroup = await this.groupsService.createOrUpdateGroup(group);
     if (updatedGroup) {
+      console.log('testign');
       await this.userService.updateUserGroups(updatedGroup.ownerId, updatedGroup.groupId);
     }
     return updatedGroup;
