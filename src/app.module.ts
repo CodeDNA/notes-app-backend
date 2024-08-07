b8c5e65e3c049ed18e4268c47cc0b6037d0e5bfc
Parker Punj <parkerpunj@macbookpro.mynetworksettings.com>
2024-08-07 04:35:11 -0400

    security updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { NotesModule } from './app/notes-api.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './authentication/auth.module';
-import { DatabaseConnectionString } from './secrets/secretsDB';
+import { DatabaseConnectionString } from './secrets/db-secrets';
 
 @Module({
   imports: [
